34ce7a46d7c398294399bf965bfc2b9e1c0b6b58
BrunoMota18 <bvgm@cin.ufpe.br>
2020-06-10 01:20:23 -0300

    init project

---
+++ src/main/java/com/brunomota18/springbootactuatorexample/model/Greeting.java
@@ -0,0 +1,12 @@
+package com.brunomota18.springbootactuatorexample.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Greeting {
+    private final long id;
+    private final String content;
+    
+}
---
+++ src/test/java/com/brunomota18/springbootactuatorexample/ActuatorServiceTests.java
@@ -0,0 +1,13 @@
+package com.brunomota18.springbootactuatorexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
