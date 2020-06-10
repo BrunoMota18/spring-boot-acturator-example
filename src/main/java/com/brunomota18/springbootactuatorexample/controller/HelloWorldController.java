93857faf20f1d8270750301bd88e7bacf71453f8
Adilson Angelo <aasj2@cin.ufpe.br>
2020-06-10 11:02:43 -0300

    Testes de endpoints

--- src/main/java/com/brunomota18/springbootactuatorexample/controller/HelloWorldController.java
+++ src/main/java/com/brunomota18/springbootactuatorexample/controller/HelloWorldController.java
@@ -11,13 +11,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
+
     private final String template = "Hello %s";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/hello-world")
     @ResponseBody
-    public Greeting sayHello(@RequestParam(name="name", required = false, defaultValue = "Stranger") String name){
+    public Greeting sayHello(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
-    
+
 }
--- src/main/java/com/brunomota18/springbootactuatorexample/model/Greeting.java
+++ src/main/java/com/brunomota18/springbootactuatorexample/model/Greeting.java
@@ -1,12 +1,21 @@
 package com.brunomota18.springbootactuatorexample.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
 public class Greeting {
+
     private final long id;
     private final String content;
-    
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
 }
